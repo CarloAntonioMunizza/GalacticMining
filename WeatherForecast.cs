bffce94fe4ab0f68f6fd3e874b9de0ca3c7f918a
Carlo_Munizza <cam7134@psu.edu>
2024-05-07 22:39:17 -0400

    test

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -6,7 +6,7 @@ public class WeatherForecast
 
     public int TemperatureC { get; set; }
 
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.62342456);
 
     public string? Summary { get; set; }
 }
